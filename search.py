8617ce20f883a672fd06a09b0c042119201bae7f
Matthew Sy <matthewpesy@gmail.com>
2019-04-07 00:37:12 +1100

    Added A star that doesnt work correctly yet

--- search.py
+++ search.py
@@ -9,6 +9,7 @@ import sys
 import json
 import math
 import operator
+from queue import PriorityQueue
 
 def main():
 
@@ -19,9 +20,9 @@ def main():
     board = initial_board(data)
 
     print_board(board, message="", debug=True)
-    manhattan_distance(data.get("pieces")[0], (-3,3), data.get("colour") )
 
-    possible_moves((0, -2), board, data.get("colour"))
+
+    a_star_search(board, (0,-1), data["colour"])
 
 
 
@@ -38,16 +39,18 @@ def initial_board(data):
     return board
 
 #Heuristic to be ised in A*
-def manhattan_distance(start_pos, current_pos, color):
+def manhattan_distance(current_pos, colour):
     end_points = {  "red" : [(3,-3),(3,-2),(3,-1),(3,0)],
                     "green" : [(-3,3),(-2,3),(-1,3),(0,3)],
                     "blue" : [(-3,0),(-2,-1),(-1,-2),(0,-3)]
     }
     distances = []
-    for i in end_points.get(color):
+    for i in end_points.get(colour):
         #euclidean = math.sqrt( (current_pos[0]-i[0])**2 + (current_pos[1]-i[1])**2 )
         manhattan = abs(current_pos[0]-i[0]) + abs(current_pos[1]-i[1])
         distances.append(manhattan)
+
+    return min(distances)
 
 #Calcualtes the possible moves excluding blocked tiles and including jumps
 def possible_moves(current_pos, board, colour):
@@ -74,20 +77,40 @@ def possible_moves(current_pos, board, colour):
                     else:
                         next_pos.append(temp_pos)
 
-    print(next_pos)
+        return next_pos
 
 
+def a_star_search(board, start, colour):
+    cost = 1
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from['start'] = None
+    cost_so_far['start'] = 0
 
-def a_star_search(board, start, end):
-    open = PriorityQueue()
-    closed = []
-    total_cost_f = {}
-    current_cost_g = {}
+    end_points = {  "red" : [(3,-3),(3,-2),(3,-1),(3,0)],
+                    "green" : [(-3,3),(-2,3),(-1,3),(0,3)],
+                    "blue" : [(-3,0),(-2,-1),(-1,-2),(0,-3)]
+                    }
 
-    total_cost_f
+    while not frontier.empty():
+        current = frontier.get()
 
+        if current in end_points[colour]:
+            break
 
+        for next in possible_moves(start, board, colour):
+            new_cost = cost_so_far['start'] + cost
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + manhattan_distance(next, colour)
+                frontier.put(next, priority)
+                came_from[next] = current
 
+    print ("This is the came_from:",came_from ,"\n")
+
+    return came_from, cost_so_far
 
 
 
